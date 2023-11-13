292ce14e3e2ce5f0e8f2d906274292446a11da24
Rohit Kalshetty <rohitshetty1998@gmail.com>
2023-11-13 16:49:15 +0100

    Merge remote changes into main

---
+++ setup.py
@@ -0,0 +1,25 @@
+Setup Git on your Windows Computer cuz Mac is Meh:
+
+For Windows:
+
+1. Download Git:
+
+	a. Visit the official Git website: https://git-scm.com/downloads
+	b. The website will detect your operating system. Click on the Windows download link.
+
+2. Run the Installer:
+
+	a.Once the download is complete, run the installer
+	b. You'll go through several installation steps. You can generally choose the default settings, but you might want to make 	sure that "Git Bash" is selected to install, as it provides a Unix-like command-line environment on Windows.
+
+3. Adjust System PATH (if prompted):
+
+	a. During the installation, there might be an option to "Adjusting your PATH environment" or "Use Git from the Windows 	Command Prompt." Choose this option to add Git to your PATH so that you can use Git from the command line.
+
+4. Finish Installation:
+
+	a. Complete the installation by following the on-screen instructions.
+
+5. Verify Installation:
+
+	a. Open a new command prompt window and type git --version. If Git has been installed successfully, it will display the 	installed version.
