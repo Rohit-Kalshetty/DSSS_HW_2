55d86409ac3329f36383df07676c5ecaf6b56547
Rohit-Kalshetty <150559022+Rohit-Kalshetty@users.noreply.github.com>
2023-11-13 21:53:50 +0100

    tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -13,22 +13,25 @@ class TestMathGame(unittest.TestCase):
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        result = function_B()
-        self.assertGreaterEqual(result, 0)
+        # TODO
+        operators = ['+', '-', '*', '/']
+        for _ in range(1000):  # Test a large number of random values
+            rand_operator = function_B()
+            self.assertIn(rand_operator, operators)
         pass
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                (10, 3, '-', '10 - 3', 7),
-                (6, 2, '*', '6 * 2', 12),
-            ]
+                (8, 3, '-', '8 - 3', 5),
+            (4, 6, '*', '4 * 6', 24),
+        ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                result_problem, result_answer = function_C(num1, num2, operator)
-                self.assertEqual(result_problem, expected_problem)
-                self.assertEqual(result_answer, expected_answer)
-                pass
+                # TODO
+                result = function_C(num1, num2, operator)
+                self.assertEqual(result, (expected_problem,expected_answer))
+            pass
 
 if __name__ == "__main__":
     unittest.main()
